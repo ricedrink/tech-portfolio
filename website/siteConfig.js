42e1ae1f832db3ab2cd7be76d5b8ddf05aad5830
Alice Margo Zhang <alicezhang32092@knights.ucf.edu>
2021-03-10 21:24:59 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -43,7 +43,7 @@ const siteConfig = {
 
     /* Colors for website */
     colors: {
-        primaryColor: '#778899',
+        primaryColor: '#000000',
         secondaryColor: '#FFFFFF',
     },
     //#778899
